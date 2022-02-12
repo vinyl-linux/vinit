980f0f0602dcee1db6673a226f8a56a6a39be5aa
jspc <james+git@zero-internet.org.uk>
2022-02-12 16:04:55 +0000

    Load service config file for service, cron, and oneoff

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/vinyl-linux/vinit
 
 go 1.17
+
+require (
+	github.com/BurntSushi/toml v1.0.0
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
+	github.com/robfig/cron/v3 v3.0.1
+)
---
+++ service_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestService(t *testing.T) {
+	for _, test := range []struct {
+		name        string
+		fn          string
+		expectError bool
+	}{
+		{"service happy path", "testdata/services/00-app/.config.toml", false},
+		{"cronjob happy path", "testdata/services/00-app-cronjob/.config.toml", false},
+		{"oneoff happy path", "testdata/services/00-app-oneoff/.config.toml", false},
+
+		// edge cases and errors
+		{"invalid type", "testdata/erroring/wrong-type.toml", true},
+		{"missing cron schedule", "testdata/erroring/missing-cron.toml", true},
+		{"invalid cron schedule", "testdata/erroring/invalid-cron.toml", true},
+		{"missing oneoff", "testdata/erroring/missing-oneoff.toml", true},
+		{"missing group name", "testdata/erroring/missing-groupname.toml", true},
+		{"missing args is fine", "testdata/successing/missing-args.toml", false},
+		{"missing user sets user to root", "testdata/successing/missing-user.toml", false},
+		{"empty validcodes gets a default", "testdata/successing/empty-validcodes.toml", false},
+
+		// minimal viable configs
+		{"minimal viable service", "testdata/mvs/service.toml", false},
+		{"minimal viable cronjob", "testdata/mvs/cron.toml", false},
+		{"minimal viable oneoff", "testdata/mvs/oneoff.toml", false},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := LoadService(test.fn)
+
+			if test.expectError && err == nil {
+				t.Errorf("expected error, received none")
+			} else if !test.expectError && err != nil {
+				t.Errorf("unexpected error %#v", err)
+			}
+		})
+	}
+}
